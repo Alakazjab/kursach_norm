5a15d5d83f2f6bd94b7f3155c120e63cc179fb19
Alakazjab <alakazjab@gmail.com>
2022-12-07 15:24:00 +0300

    commit_hash

--- kursach/Login.cs
+++ kursach/Login.cs
@@ -34,15 +34,17 @@ namespace kursach
                     con.ConnectionString = "Server = 172.20.8.6;Port=5432;User Id=st0901;Password=pwd0901;Database=st0901_08";
                     NpgsqlCommand cmd = new NpgsqlCommand();
                     cmd.Connection = con;
-                    cmd.CommandType = CommandType.Text;
                     cmd.CommandText = 
                         "Select * from kursach.users where email=\'"
-                        + textBox1.Text +"\' and password =\'" + textBox2.Text + "\';";
+                        + textBox1.Text +"\' and password =\'" + BCrypt.Net.BCrypt.HashPassword(textBox2.Text) + "\';";
                     con.Open();
                     NpgsqlDataReader rdr = cmd.ExecuteReader();
                     ArrayList records = new ArrayList();
                     if (rdr.HasRows)
                     {
+                        foreach (DbDataRecord rec in rdr)
+                            records.Add(rec);
+                        con.Close();
                         childForm1 = new MainForm(data);
                         childForm1.Show();
                     }
@@ -54,8 +56,8 @@ namespace kursach
                         DialogResult result;
                         // Displays the MessageBox.
                         result = MessageBox.Show(message, caption, buttons);
-                    }
-                    con.Close();
+                        con.Close();
+                    }         
                 }
                 else
                 {
