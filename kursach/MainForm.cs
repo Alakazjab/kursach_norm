43a6f37ac21ee970229ecbb44b213c10bd18f1ba
Alakazjab <alakazjab@gmail.com>
2022-12-09 04:22:57 +0300

    one_more_commit

--- kursach/MainForm.cs
+++ kursach/MainForm.cs
@@ -15,6 +15,7 @@ namespace kursach
 {
     public partial class MainForm : Form
     {
+        AdminForm childForm;
         public string a;
         public MainForm(string data)
         {
@@ -28,18 +29,37 @@ namespace kursach
             con.ConnectionString = "Server = 172.20.8.6;Port=5432;User Id=st0901;Password=pwd0901;Database=st0901_08";
             NpgsqlCommand cmd = new NpgsqlCommand();
             cmd.Connection = con;
-            cmd.CommandType = CommandType.StoredProcedure;
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = $"select * from kursach.student_info where email = {a}";
-
+            cmd.CommandText = $"select role from kursach.users where email = {a}";
             con.Open();
             NpgsqlDataReader rdr = cmd.ExecuteReader();
-            ArrayList records = new ArrayList();
-            if (rdr.HasRows)
-                foreach (DbDataRecord rec in rdr)
-                    records.Add(rec);
-            con.Close();
-            dataGridView1.DataSource = records;
+            rdr.Read();
+            if (rdr.GetString(0)=="admin")
+            {
+                childForm = new AdminForm(a);
+                childForm.Show();
+                con.Close();
+                rdr.Close();
+            }
+            else
+            {
+                cmd.CommandText = $"select date, disciplina, forma_controlya, ocenka, prepod from kursach.student_info where email = {a}";
+                con.Open();
+                rdr = cmd.ExecuteReader();
+                ArrayList records = new ArrayList();
+                if (rdr.HasRows)
+                    foreach (DbDataRecord rec in rdr)
+                        records.Add(rec);
+                con.Close();
+                rdr.Close();
+                cmd.CommandText = $"select stipendia from kursach.student_info where email = {a}";
+                con.Open();
+                cmd.ExecuteReader();
+                rdr.Read();
+                label1.Text += rdr.GetString(0);
+                con.Close();
+                dataGridView1.DataSource = records;
+            }
+            
         }
     }
 }
