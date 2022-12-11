23e4bd90b3c0fd1d91b18cabcd65843b5d6e72de
Alakazjab <alakazjab@gmail.com>
2022-12-11 13:54:45 +0300

    add_sort

--- kursach/AdminForm.Designer.cs
+++ kursach/AdminForm.Designer.cs
@@ -30,11 +30,119 @@ namespace kursach
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.сортировкаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.поУбываниюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.поВозрастаниюToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сортировкаToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripTextBox();
+            this.отсортироватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(798, 383);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.ColumnHeaderMouseDoubleClick += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_ColumnHeaderMouseDoubleClick);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сортировкаToolStripMenuItem1});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сортировкаToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(139, 26);
+            // 
+            // сортировкаToolStripMenuItem
+            // 
+            this.сортировкаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.поУбываниюToolStripMenuItem,
+            this.поВозрастаниюToolStripMenuItem});
+            this.сортировкаToolStripMenuItem.Name = "сортировкаToolStripMenuItem";
+            this.сортировкаToolStripMenuItem.Size = new System.Drawing.Size(138, 22);
+            this.сортировкаToolStripMenuItem.Text = "сортировка";
+            // 
+            // поУбываниюToolStripMenuItem
+            // 
+            this.поУбываниюToolStripMenuItem.Name = "поУбываниюToolStripMenuItem";
+            this.поУбываниюToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.поУбываниюToolStripMenuItem.Text = "по убыванию";
+            // 
+            // поВозрастаниюToolStripMenuItem
+            // 
+            this.поВозрастаниюToolStripMenuItem.Name = "поВозрастаниюToolStripMenuItem";
+            this.поВозрастаниюToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.поВозрастаниюToolStripMenuItem.Text = "по возрастанию";
+            // 
+            // сортировкаToolStripMenuItem1
+            // 
+            this.сортировкаToolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1,
+            this.отсортироватьToolStripMenuItem});
+            this.сортировкаToolStripMenuItem1.Name = "сортировкаToolStripMenuItem1";
+            this.сортировкаToolStripMenuItem1.Size = new System.Drawing.Size(85, 20);
+            this.сортировкаToolStripMenuItem1.Text = "Сортировка";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(100, 23);
+            // 
+            // отсортироватьToolStripMenuItem
+            // 
+            this.отсортироватьToolStripMenuItem.Name = "отсортироватьToolStripMenuItem";
+            this.отсортироватьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.отсортироватьToolStripMenuItem.Text = "Отсортировать";
+            this.отсортироватьToolStripMenuItem.Click += new System.EventHandler(this.сортировкаToolStripMenuItem1_Click);
+            // 
+            // AdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "AdminForm";
             this.Text = "AdminForm";
+            this.Load += new System.EventHandler(this.AdminForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem сортировкаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem поУбываниюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem поВозрастаниюToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem сортировкаToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripTextBox toolStripTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem отсортироватьToolStripMenuItem;
     }
 }
--- kursach/AdminForm.cs
+++ kursach/AdminForm.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using kursach.Properties;
+using Npgsql;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Common;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,6 +21,83 @@ namespace kursach
         {
             InitializeComponent();
             a = data;
+        }
+        private void AdminForm_Load(object sender, EventArgs e)
+        {
+            NpgsqlConnection con = new NpgsqlConnection();
+            con.ConnectionString = "Server = 172.20.8.6;Port=5432;User Id=st0901;Password=pwd0901;Database=st0901_08";
+            NpgsqlCommand cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = $"select * from kursach.student;";
+            con.Open();
+            NpgsqlDataReader rdr = cmd.ExecuteReader();
+            ArrayList records = new ArrayList();
+            if (rdr.HasRows)
+                foreach (DbDataRecord rec in rdr)
+                    //dataSet1.Tables.(rec);
+                    records.Add(rec);
+            con.Close();
+            dataGridView1.DataSource = records;
+            
+        }
+
+        private void dataGridView1_ColumnHeaderMouseDoubleClick(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            /*string message = Convert.ToString(dataGridView1.ColumnHeaderCellChanged).Trim(new char[] { '{', '}' });
+            string caption = "Ошибка входа";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            DialogResult result;
+            // Displays the MessageBox.
+            result = MessageBox.Show(message, caption, buttons);
+            NpgsqlConnection con = new NpgsqlConnection();
+            con.ConnectionString = "Server = 172.20.8.6;Port=5432;User Id=st0901;Password=pwd0901;Database=st0901_08";
+            NpgsqlCommand cmd = new NpgsqlCommand();
+            cmd.Connection = con;
+            cmd.CommandText = $"select * from kursach.student order by "+"id" +";";
+            con.Open();
+            NpgsqlDataReader rdr = cmd.ExecuteReader();
+            ArrayList records = new ArrayList();
+            if (rdr.HasRows)
+                foreach (DbDataRecord rec in rdr)
+                    //dataSet1.Tables.(rec);
+                    records.Add(rec);
+            con.Close();
+            dataGridView1.DataSource = records;*/
+        }
+
+        private void сортировкаToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+               NpgsqlConnection con = new NpgsqlConnection();
+                con.ConnectionString = "Server = 172.20.8.6;Port=5432;User Id=st0901;Password=pwd0901;Database=st0901_08";
+                NpgsqlCommand cmd = new NpgsqlCommand();
+                cmd.Connection = con;
+                cmd.CommandText = $"select * from kursach.student order by " + toolStripTextBox1.Text + ";";
+                con.Open();
+                NpgsqlDataReader rdr = cmd.ExecuteReader();
+                ArrayList records = new ArrayList();
+                if (rdr.HasRows)
+                    foreach (DbDataRecord rec in rdr)
+                        //dataSet1.Tables.(rec);
+                        records.Add(rec);
+                con.Close();
+                dataGridView1.DataSource = records;
+                toolStripTextBox1.Clear();
+            }
+            catch
+            {
+                string message = "Неверно введено название колонки";
+                string caption = "Ошибка сортировки";
+                MessageBoxButtons buttons = MessageBoxButtons.OK;
+                DialogResult result;
+                // Displays the MessageBox.
+                result = MessageBox.Show(message, caption, buttons);
+            }
+            finally
+            {
+                toolStripTextBox1.Clear();
+            }
         }
     }
 }
